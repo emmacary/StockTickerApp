e788c724549c7cc8855723604888d33ac68ee5f1
emmacary <emma.cary@tufts.edu>
2021-04-25 22:42:31 -0400

    webapp

---
+++ process.js
@@ -0,0 +1,69 @@
+var http = require('http');
+var port = process.env.PORT || 3000;
+var fs = require('fs');
+var qs = require('querystring');
+
+http.createServer(function (req,res)
+{
+	//Get form data
+	if (req.url == "/process") {
+		res.writeHead(200, {'Content-Type': 'text/html'});  
+		pdata = "";
+		req.on('data', data => {
+			pdata += data.toString();
+		});
+
+		req.on('end', () => {
+			pdata = qs.parse(pdata);
+			search = pdata['search'];
+			type = pdata['c_or_t'];
+			res.write("You are searching for " + search + "<br><br>");
+			res.write("Your results are: " + "<br>");
+			
+			//Connect to database
+			var mongo = require('mongodb');
+			var MongoClient = mongo.MongoClient;
+			const url = "mongodb+srv://emmacary17:CBSWP151515@cluster0.dzzcs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+			MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {    
+				if(err) { 
+					console.log("Connection err: " + err); 
+					return; 
+				}    
+				var dbo = db.db("companies");    
+				var collection = dbo.collection("companies");   
+			
+			    //Call the query
+			    if (type == "Company") {
+			    	theQuery = {Company: search};
+			    }
+			    if (type == "Ticker") {
+			    	theQuery = {Ticker: search};
+			    }
+			    
+				collection.find(theQuery).toArray(function(err, items) {  
+					if (err) {
+						console.log("Error: " + err);  
+					} 
+					else {
+						if (items.length == 0){
+							res.write("None found! Either check your search for spelling mistakes, or the search you're looking for doesn't exist!");
+						}
+						else {
+							for (i=0; i<items.length; i++) {
+								res.write(items[i].Company + " has ticker " + items[i].Ticker + "<br>");  
+							}
+						}
+						res.end();
+					}     
+					db.close();
+		        });
+	        });
+		});
+	}
+	else{
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write ("Unknown page request");
+		res.end();  
+	}
+}).listen(port);
